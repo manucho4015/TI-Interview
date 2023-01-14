5726b3caf17ec99631be4067b04d995bbb77ca59
Emmanuel Ochieng <eochieng16@gmail.com>
2023-01-14 19:51:52 +0300

    configure redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import homeReducer from "./features/home/homeSlice";
 
+// import reducers
+import homeReducer from "./features/home/homeSlice";
+import formReducer from "./features/form/formSlice";
+import modalReducer from "./features/modal/modalSlice";
+
+// create redux store
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    form: formReducer,
+    modal: modalReducer,
   },
 });
